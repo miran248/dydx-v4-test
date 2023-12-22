13554049850c937adc726b883026085d96cd5992
miran248 <miran@248.sh>
2023-12-22 13:22:43 +0100

    chore: use type registry

--- validator-client.ts
+++ validator-client.ts
@@ -1,37 +1,34 @@
-import { decodeTxRaw } from "@cosmjs/proto-signing";
-import { Any, ValidatorClient } from "@dydxprotocol/v4-client-js";
+import { Registry, decodeTxRaw } from "@cosmjs/proto-signing";
+import { ValidatorClient } from "@dydxprotocol/v4-client-js";
 import * as proto from "@dydxprotocol/v4-proto/src/codegen";
 import { network } from "./network";
 
-const decodeMessage = (message: Any) => {
-  switch (message.typeUrl) {
-    case "/dydxprotocol.clob.MsgProposedOperations":
-      return proto.dydxprotocol.clob.MsgProposedOperations.decode(
-        message.value
-      );
-    case "/dydxprotocol.bridge.MsgAcknowledgeBridges":
-      return proto.dydxprotocol.bridge.MsgAcknowledgeBridges.decode(
-        message.value
-      );
-    case "/dydxprotocol.perpetuals.MsgAddPremiumVotes":
-      return proto.dydxprotocol.perpetuals.MsgAddPremiumVotes.decode(
-        message.value
-      );
-    case "/dydxprotocol.prices.MsgUpdateMarketPrices":
-      return proto.dydxprotocol.prices.MsgUpdateMarketPrices.decode(
-        message.value
-      );
-    default:
-      throw new Error(`unknown message type ${message.typeUrl}`);
-  }
-};
+const registry = new Registry([
+  [
+    "/dydxprotocol.clob.MsgProposedOperations",
+    proto.dydxprotocol.clob.MsgProposedOperations,
+  ],
+  [
+    "/dydxprotocol.bridge.MsgAcknowledgeBridges",
+    proto.dydxprotocol.bridge.MsgAcknowledgeBridges,
+  ],
+  [
+    "/dydxprotocol.perpetuals.MsgAddPremiumVotes",
+    proto.dydxprotocol.perpetuals.MsgAddPremiumVotes,
+  ],
+  [
+    "/dydxprotocol.prices.MsgUpdateMarketPrices",
+    proto.dydxprotocol.prices.MsgUpdateMarketPrices,
+  ],
+]);
+
 const decodeTransaction = (transaction: Uint8Array) => {
   const decoded = decodeTxRaw(transaction);
 
   for (const message of decoded.body.messages) {
     console.log("decodeTransaction message", message);
 
-    const operation = decodeMessage(message);
+    const operation = registry.decode(message);
 
     console.log("decodeTransaction operation", operation);
   }
